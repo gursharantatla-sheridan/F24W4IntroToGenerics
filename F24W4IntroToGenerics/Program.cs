c2e276a6d820875465fbd9ade3da3273c7f2f899
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-09-24 14:29:33 -0400

    generic example

--- F24W4IntroToGenerics/Program.cs
+++ F24W4IntroToGenerics/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // boxing
+            int num = 5;
+            object obj = num;
+
+            // unboxing
+            num = (int)obj;
+
+
+            //if (AreEqual(5, 5))
+            if (AreEqual<int>(5, 5))
+                Console.WriteLine("Both are equal");
+            else
+                Console.WriteLine("Both are not equal");
+        }
+
+        // standard method
+        static bool AreEqual(object a, object b)
+        {
+            return a.Equals(b);
+        }
+
+        // generic method
+        static bool AreEqual<T>(T a, T b)
+        {
+            return a.Equals(b);
         }
     }
 }
