1224b2c5ea1307c7c8535ff1f29caa7ad4c5cc49
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-09-26 13:44:04 -0400

    stack example

---
+++ StackAndQueueExample/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace StackAndQueueExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Stack Example
+            Console.WriteLine("Stack Example\n");
+
+            Stack<int> stack = new Stack<int>();
+            stack.Push(10);
+            stack.Push(20);
+            stack.Push(30);
+
+            //int num = stack[1];
+
+            Console.WriteLine("Total items: " + stack.Count);
+
+            foreach (int i in stack)
+                Console.WriteLine(i);
+
+            int num = stack.Pop();
+            Console.WriteLine("\nNum = " + num);
+
+            Console.WriteLine("\nTotal items: " + stack.Count);
+
+            foreach (int i in stack)
+                Console.WriteLine(i);
+
+
+            num = stack.Peek();
+            Console.WriteLine("\nNum = " + num);
+
+            Console.WriteLine("\nTotal items: " + stack.Count);
+
+            foreach (int i in stack)
+                Console.WriteLine(i);
+        }
+    }
+}
