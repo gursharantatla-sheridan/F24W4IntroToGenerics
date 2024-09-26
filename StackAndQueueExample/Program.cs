36b1a603255d5a94c74fc20932b8dd7ea4e4de7b
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-09-26 13:52:27 -0400

    queue example

--- StackAndQueueExample/Program.cs
+++ StackAndQueueExample/Program.cs
@@ -35,6 +35,38 @@
 
             foreach (int i in stack)
                 Console.WriteLine(i);
+
+
+            // Queue Example
+            Console.WriteLine("\n\n\nQueue Example\n");
+
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(10);
+            queue.Enqueue(20);
+            queue.Enqueue(30);
+
+            //num = queue[1];
+
+            Console.WriteLine("Total items: " + queue.Count);
+
+            foreach (int i in queue)
+                Console.WriteLine(i);
+
+            num = queue.Dequeue();
+            Console.WriteLine("\nNum = " + num);
+
+            Console.WriteLine("\nTotal items: " + queue.Count);
+
+            foreach (int i in queue)
+                Console.WriteLine(i);
+
+            num = queue.Peek();
+            Console.WriteLine("\nNum = " + num);
+
+            Console.WriteLine("\nTotal items: " + queue.Count);
+
+            foreach (int i in queue)
+                Console.WriteLine(i);
         }
     }
 }
